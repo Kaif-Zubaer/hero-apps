594672254cbfb53a2cb33f360b87d4ec2c334bfc
Kaif-Zubaer <kaifhossainzubaer@gmail.com>
2025-10-12 18:23:11 +0600

    navbar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import logo from '../../assets/logo.png';
+import gitImg from '../../assets/github-brands-solid-full.svg';
+import barIcon from '../../assets/bars-solid-full.svg';
+import { Link, NavLink } from 'react-router';
+import './Navbar.css';
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(prev => !prev);
+    };
+
     return (
-        <div>
-            
+        <div className='navbar'>
+            <div className='navbar-left-section'>
+                <img className='bar-icon' src={barIcon} alt="menu" onClick={toggleMenu} />
+                <Link className='navbar-left' to='/'>
+                    <div className='navbar-left'>
+                        <img className='navbar-logo-img' src={logo} alt="logo" />
+                        <h1 className='navbar-brand-title'>HERO.IO</h1>
+                    </div>
+                </Link>
+            </div>
+
+            <div className={`navbar-middle ${menuOpen ? 'open' : ''}`}>
+                <ul>
+                    <NavLink className='link-deco' to='/'><li>Home</li></NavLink>
+                    <NavLink className='link-deco' to='apps'><li>Apps</li></NavLink>
+                    <NavLink className='link-deco' to='installation'><li>Installation</li></NavLink>
+                </ul>
+            </div>
+
+            <div className='navbar-right'>
+                <a className='navbar-btn-link' href="https://github.com/Kaif-Zubaer" target='_blank' rel='noopener noreferrer'>
+                    <button className='navbar-btn'>
+                        <img src={gitImg} alt="GitHub" />
+                        Contribute
+                    </button>
+                </a>
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { RouterProvider } from "react-router/dom";
 import Root from './components/Root/Root';
 import Home from './components/Home/Home';
 import ErrorPage from './components/ErrorPage/ErrorPage';
+import Apps from './components/Apps/Apps';
+import Installation from './components/Installation/Installation';
 
 const router = createBrowserRouter([
   {
@@ -15,8 +17,17 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
+        path: '/',
         Component: Home,
-      }
+      },
+      {
+        path: 'apps',
+        Component: Apps,
+      },
+      {
+        path: 'installation',
+        Component: Installation,
+      },
     ]
   },
 ]);
